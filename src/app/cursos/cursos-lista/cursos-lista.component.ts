3e5ae3402c762683ed9e36ef933165af669a589a
Eronheit <aquinoeronaldo@gmail.com>
2021-10-07 17:43:24 -0300

    feat: http capture errors + error with async

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { empty, Observable, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -13,12 +14,24 @@ export class CursosListaComponent implements OnInit {
 
   //cursos!: Curso[];
   cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private cursosService: CursosService) { }
 
-  ngOnInit(): void {
+  onRefresh() {
     //this.cursosService.list().subscribe(data => this.cursos = data)
-    this.cursos$ = this.cursosService.list();
+    this.cursos$ = this.cursosService.list()
+      .pipe(
+        catchError(error => {
+          console.log(error)
+          this.error$.next(true);
+          return empty();
+        })
+      );
+  }
+
+  ngOnInit(): void {
+    this.onRefresh();
   }
 
 }
