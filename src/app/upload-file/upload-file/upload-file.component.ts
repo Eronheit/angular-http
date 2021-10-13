942fbb212db2eb068b89f39aa3328bd89f84cb06
Eronheit <aquinoeronaldo@gmail.com>
2021-10-13 10:42:54 -0300

    feat: http - upload files - Request with FormData

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -7,16 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  files!: Set<File>;
+
+  constructor(private uploadFileService: UploadFileService) { }
 
   ngOnInit(): void {
   }
 
   onChange(event: any) {
-    const selectedFiles: FileList = event.srcElement.files;
+    //const selectedFiles: FileList = event.srcElement.files;
+
+    this.files = new Set();
 
     const fileNames =  [...event.srcElement.files].map((file) => `${file.name}`).join(', ');
 
+    [...event.srcElement.files].map(file => this.files.add(file));
+
     document.getElementById('customFileLabel')!.innerHTML = fileNames;
+  }
+
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.uploadFileService.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe((response) => console.log('Upload Concluído', response));
+    }
   }
 }
