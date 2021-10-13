a5a25f132ad7dc3528a398693668d767e62b61c9
Eronheit <aquinoeronaldo@gmail.com>
2021-10-13 12:19:41 -0300

    feat: http - remove CORS of backend and use angular proxy

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { UploadFileService } from '../upload-file.service';
 
 @Component({
@@ -29,7 +30,7 @@ export class UploadFileComponent implements OnInit {
 
   onUpload() {
     if (this.files && this.files.size > 0) {
-      this.uploadFileService.upload(this.files, 'http://localhost:8000/upload')
+      this.uploadFileService.upload(this.files, `${environment.BASE_URL}/upload`)
         .subscribe((response) => console.log('Upload Concluído', response));
     }
   }
